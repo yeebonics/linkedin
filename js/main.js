45059aab35c11a1f0d228b27a59a30d2240c9315
yeebonics <darrentyee@gmail.com>
2020-10-01 23:09:56 -0700

    Add files via upload
    First Upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(".pill").on("click", function () {
+  var pillText = $(this).children("p").text();
+  $(".pill").removeClass("active");
+  $(this).addClass("active");
+
+  $(".cal-container").removeClass("active");
+  $("#" + pillText).addClass("active");
+  
+})
+
+$("#chat2").on("click", function () {
+  $(this).removeClass("notification");
+  $("#chat-notification").css("display", "none")
+})
+
+$(".chat").on("click", function () {
+  $(this).toggleClass("active");
+})
+
+$(".meeting-pill").on("click", function (e) {
+  e.stopPropagation();
+  if ($(this).attr("id") == "pill-accept") {
+    $("#scheduled1").addClass("active")
+    $(".pill, .cal-container").removeClass("active")
+    $(".pill:nth-child(2), .cal-container:nth-child(2)").addClass("active");
+    $("#empty-schedule").remove();
+    $("#confirmation2").text("You accepted Jeffrey's meeting invitation")
+
+  } else {
+    $("#confirmation2").text("You declined Jeffrey's meeting invitation")
+  }
+  $("#meeting2").css("display", "none");
+  $(".meeting-pill-container, #empty-schedule").remove();
+ 
+ 
+})
+
+$("#submit").on("click", function () {
+  if ($(this).hasClass("cancel")) {
+    return false;
+  }
+  $(".btn-text").text("Scheduling");
+  $("#loader").addClass("active");
+  setTimeout(() => {
+    $("#loader").removeClass("active");
+    $(".btn-text").text("Success");
+    $("#success-check").addClass("active");
+  }, 1000);
+
+  // reset form
+  setTimeout(() => {
+    $("#input-event").text("Enter event name")
+    $("#input-desc").text("Enter a short description for your meeting")
+    $("#form-date").text("Today")
+    $("#form-time").text("12:00")
+    $("#form-hour").text("Hour")
+    $("#form-minutes").text("Minutes")
+
+    $(".input-pill").remove();
+    $(".radio-select").removeClass("active")
+    $("#input-event, #input-desc").addClass("inactive");
+  }, 1000);
+
+  setTimeout(() => {
+    var currentTime = new Date().toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit' });
+
+    $("#submit").text("Schedule");
+    $("#meeting-message, .meeting-confirmation").addClass("active");
+    $("#meeting1").css("display", "flex");
+    $("#Pending > .holder-text").remove();
+    $("#time1").text(currentTime);
+    $("#chat1").addClass("active");
+    $("#meeting-message")[0].scrollIntoView(true);
+    $("#test").scrollTop(-100);
+    $("#submit").addClass("cancel")
+  }, 2000);
+
+  setTimeout(() => {
+    var currentTime = new Date().toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit' });
+    $("#chat2").css("display", "block")
+    $("#meeting2").css("display", "flex")
+  }, 6000);
+})
+
